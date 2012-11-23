5e8d0d859d25ffaa8b5c813e6ddcce9e800c02e6
maiah <jjmmacariola@gmail.com>
2012-11-23 23:24:54 +0800

    Added doc comments to explain the implemantation

--- index.js
+++ index.js
@@ -4,28 +4,30 @@ var timestamps = require('model-timestamps');
 var View = require('view');
 var domify = require('domify');
 
+/*
+ * The model declaration.
+ */
 var Person = model('Person')
   .attr('firstName')
   .attr('lastName')
   .use(timestamps);
 
-var changeName = function() {
-  if (maiah.firstName() === 'Maiah') {
-	  maiah.firstName('James');
-	} else {
-	  maiah.firstName('Maiah');
-	}
-};
-
-var maiah = new Person({ "firstName": "Maiah", "lastName": "Macariola" });
-
+/*
+ * The view declaration.
+ */
 var PersonView = function(person) {
 	View.call(this, person, domify(personTmpl));
 	this.bind('click .changeBtn', 'changeName');
 };
 
+/*
+ * Inherit from View prototype.
+ */
 PersonView.prototype.__proto__ = View.prototype;
 
+/*
+ * The view function declaration.
+ */
 PersonView.prototype.changeName = function() {
   if (maiah.firstName() === 'Maiah') {
 	  maiah.firstName('James');
@@ -34,6 +36,14 @@ PersonView.prototype.changeName = function() {
 	}
 };
 
+/*
+ * Create person and view object and bind them together.
+ */
+var maiah = new Person({ "firstName": "Maiah", "lastName": "Macariola" });
 var personView = new PersonView(maiah);
+
+/*
+ * Attach the view to the "p" element in the body.
+ */
 var p = document.querySelector('p');
 p.appendChild(personView.el);
